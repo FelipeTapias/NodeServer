607af45eea1f4d08a5231f725de86f6a253a879d
FelipeTapias <tapiastuberquiados@gmail.com>
2022-03-17 15:02:59 -0500

    I deleted unnecesary comments

--- config/actionsPassword.js
+++ config/actionsPassword.js
@@ -1,16 +1,14 @@
-// Importamos paquete
 const bcrypt = require("bcryptjs");
-// Entre más rondas, mejor protección, pero más consumo de recursos. 10 está bien
-const rondasDeSal = 10;
+
+const salt = 10;
 
 const hashingPassword = (password) => {
     return  new Promise((resolve, reject) => {
-        bcrypt.hash(password, rondasDeSal, async(err, palabraSecretaEncriptada) => {
+        bcrypt.hash(password, salt, async(err, palabraSecretaEncriptada) => {
             err ? reject(err) : resolve(palabraSecretaEncriptada);
         });
     });
 }
-
 
 module.exports = {
     hashingPassword: hashingPassword
